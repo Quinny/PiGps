4908aeef425ca810b8c631b94a77a423503c2072
Quinn Perfetto <thequinnftw@gmail.com>
2021-09-19 12:20:14 -0700

    abstract out the gps device

---
+++ gpsdevice.py
@@ -0,0 +1,31 @@
+import io
+import pynmea2
+import serial
+
+# The format for NMEA coordinates is (d)ddmm.mmmm
+# where d=degrees and m=minutes (http://aprs.gids.nl/nmea/#latlong)
+#
+# There are 60 minutes in a degree, so divide the minutes by 60 and add that to
+# the degrees in order to get the standard decmial format.
+def nmea_to_decimal(nmea_str, direction):
+    start_minutes = nmea_str.index('.') - 2
+    minutes = float(nmea_str[start_minutes:])
+    degrees = int(nmea_str[:start_minutes])
+    # South and West are considered negative coordinates.
+    multipler = -1 if direction in ['W', 'S'] else 1
+    return (degrees + (minutes / 60.0)) * multipler
+
+class GpsDevice:
+    def __init__(self):
+        ser = serial.Serial('/dev/ttyS0', 9600, timeout=5.0)
+        self.serial_io = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
+    def poll_location():
+        while True:
+            line = sio.readline()
+            msg = pynmea2.parse(line)
+            if type(msg) is pynmea2.GGA:
+                return {
+                    "longitude": nmea_to_decimal(msg.lon, msg.lon_dir),
+                    "latitude": nmea_to_decimal(msg.lat, msg.lat_dir)
+                }
